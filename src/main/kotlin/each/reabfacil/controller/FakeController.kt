3b7c23395effa2b8f5e7439e490d0d82a1cf3ab8
GuiCoppini <guilhermecoppinip@gmail.com>
2018-09-19 19:52:17 -0300

    Cria FakeController pra usar como mock da api

---
+++ src/main/kotlin/each/reabfacil/controller/FakeController.kt
@@ -0,0 +1,17 @@
+package each.reabfacil.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/api")
+class FakeController {
+
+    @GetMapping("/clinicas")
+    fun listaClinicas() : List<Clinica> {
+        return listOf(Clinica("clinica1", "1112345678"),
+                Clinica("clinica2", "1198765432"))
+    }
+
+}
+
+data class Clinica(val nome: String, val telefone: String)
