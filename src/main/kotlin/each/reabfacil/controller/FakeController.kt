8ceed534291e341a66fa1f9d190eb77de6ee65c5
GuiCoppini <guilhermecoppinip@gmail.com>
2018-09-19 20:09:43 -0300

    Faz URL do FakeController ficar certa

--- src/main/kotlin/each/reabfacil/controller/FakeController.kt
+++ src/main/kotlin/each/reabfacil/controller/FakeController.kt
@@ -1,9 +1,11 @@
 package each.reabfacil.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 class FakeController {
 
     @GetMapping("/clinicas")
