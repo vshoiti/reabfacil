0e9fbf4d0f147faf8a415e71e8dd677084e5e66d
Guilherme Coppini Pavani <gpavani@uolinc.com>
2018-09-19 12:25:28 -0300

    Initial Commit

---
+++ src/main/kotlin/each/reabfacil/ReabfacilApplication.kt
@@ -0,0 +1,11 @@
+package each.reabfacil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReabfacilApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReabfacilApplication>(*args)
+}
